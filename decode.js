0d3a59c00b4cffe591aa862e8865ca73a04c10b6
sfger <sfger111@163.com>
2014-03-25 22:06:47 +0800

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,24 +1,24 @@
 "user strict";
 // fn getType {{{
 var getType = function(o){
-     return Object.prototype.toString.call(o).slice(8, -1);
+	return Object.prototype.toString.call(o).slice(8, -1);
 };
 // }}}
 
 // fn decode{{{
 var decode = function(){
-    var args = Array.prototype.slice.call(arguments),
+	var args = Array.prototype.slice.call(arguments),
 		key = args.shift(),
 		ret, iv, inv;
-    for(var i=0,ii=args.length; i<ii; i+=2){
+	for(var i=0,ii=args.length; i<ii; i+=2){
 		iv = args[i], inv = (i+1 in args) && args[i+1];
-        if(key===iv && inv!==undefined){
+		if(key===iv && inv!==undefined){
 			iv = inv;
 			break;
 		}else if((i+1)!==ii){
 			iv = undefined;
 		}
-    }
+	}
 	return typeof iv==='function' ? iv(key) : iv;
 };
 // }}}
