f4807d21a8829322da3577eb7fdf5d9d830ed4ac
vm370 <2870226+vm370@users.noreply.github.com>
2017-12-21 11:27:22 +0300

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,42 +1,21 @@
-"user strict";
-// fn getType {{{
-var getType = function(o){
-	return Object.prototype.toString.call(o).slice(8, -1);
-};
-// }}}
+function decode ()  /* like oracle decode */
+    {
+        const args = Array.prototype.slice.call(arguments);
+        const v_key = args.shift();
+        let iv;
+        	
+        for (let i=0, ii=args.length; i < ii; i += 2 ) {
+            if ( (i + 1) === ii ) {
+                iv =  args[i];
+                break;
+            }
+            
+            if ( v_key === args[i] && ( (i + 1) < ii ) ) {
+                iv = args[i+1];
+                break;
+            }
+        }
 
-// fn decode{{{
-var decode = function(){
-	var args = Array.prototype.slice.call(arguments),
-		key = args.shift(),
-		ret, iv, inv;
-	for(var i=0,ii=args.length; i<ii; i+=2){
-		iv = args[i], inv = (i+1 in args) && args[i+1];
-		if(key===iv && inv!==undefined){
-			iv = inv;
-			break;
-		}else if((i+1)!==ii){
-			iv = undefined;
-		}
-	}
-	return typeof iv==='function' ? iv(key) : iv;
-};
-// }}}
+        return typeof iv === 'function' ? iv(v_key) : iv;
+  };
 
-// fn casers{{{
-var switcher = function(key, kvs, rfn){
-	var type = getType(kvs);
-	key = decode(
-		type in {'Array':1, 'Object':1},
-		true, key,
-		function(){
-			throw new Error('Function casers Parameter `kvs` type error!');
-		}
-	);
-	var kv = kvs[key];
-	kv = kv===undefined ? rfn : kv;
-	return typeof kv=='function' ? kv(key) : kv;
-};
-// }}}
-
-// vim: fdm=marker
